6c23bfd366e88a9f378961fa670dc84829232154
九条研介 <dinopresent@qq.com>
2018-11-14 23:35:41 +0800

    地图可以跳转到列表

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -15,7 +15,7 @@
         </div>
     </movable-view>
     <div class="index-tab">
-      <div class="index-tab-item icon-map" >
+      <div class="index-tab-item icon-map" @click="bindTab('../index/main')" >
         <img src="https://gw.alicdn.com/tfs/TB1hOhEmMDqK1RjSZSyXXaxEVXa-90-101.png" alt="">
       </div>
       <div class="index-tab-item icon-list" @click="bindTab('../index/main')">
@@ -42,171 +42,183 @@
 </template>
 
 <script>
-import { config } from '../../utils/index';
+import { config } from "../../utils/index";
 
 export default {
   data() {
     return {
-      spotList:[],
-      activeSpot:0,
-      activeWindow:-1,
-      x:0,
-      y:0, 
-      mapStart:{
-        lng:114.32751775,
-        lat:22.63737202
+      spotList: [],
+      activeSpot: 0,
+      activeWindow: -1,
+      x: 0,
+      y: 0,
+      mapStart: {
+        lng: 114.32751775,
+        lat: 22.63737202
       },
-      mapEnd:{
-        lng:114.36443567,
-        lat:22.60346353
+      mapEnd: {
+        lng: 114.36443567,
+        lat: 22.60346353
       },
-      Xstart:{
-        lng:114.32751775,
-        lat:22.63737202
+      Xstart: {
+        lng: 114.32751775,
+        lat: 22.63737202
       },
-      Xend:{
-        lng:114.32751775,
-        lat:22.60346353
+      Xend: {
+        lng: 114.32751775,
+        lat: 22.60346353
       },
-      Ystart:{
-        lng:114.32751775,
-        lat:22.63737202
+      Ystart: {
+        lng: 114.32751775,
+        lat: 22.63737202
       },
-      Yend:{
-        lng:114.36443567,
-        lat:22.63737202
+      Yend: {
+        lng: 114.36443567,
+        lat: 22.63737202
       },
-      nearSpot:0,
-      prefix:config.prefix
+      nearSpot: 0,
+      prefix: config.prefix
     };
   },
-  computed: {
-
-  },
+  computed: {},
 
   components: {},
 
   methods: {
     setStorage(key, val) {
       try {
-        wx.setStorageSync(key,val)
-      } catch(e) {
-        wx.setStorage(key,val)
+        wx.setStorageSync(key, val);
+      } catch (e) {
+        wx.setStorage(key, val);
       }
     },
     getStorage(key) {
       try {
-        wx.getStorageSync(key)
-      } catch(e) {
-        wx.getStorage(key)
+        wx.getStorageSync(key);
+      } catch (e) {
+        wx.getStorage(key);
       }
     },
-    distance(item){
-      let x = this.locate(this.Xstart,this.Xend)
-      let y = this.locate(this.Ystart,this.Yend)
-      let width = 3753
-      let spotX = {},spotY = {}
-      spotX.lng = this.Xstart.lng
-      spotX.lat = item.latitude
-      let left = this.locate(this.Xstart,spotX)
-      spotY.lng = item.longitude
-      spotY.lat = this.Ystart.lat
-      let top = this.locate(this.Ystart,spotY)
+    distance(item) {
+      let x = this.locate(this.Xstart, this.Xend);
+      let y = this.locate(this.Ystart, this.Yend);
+      let width = 3753;
+      let spotX = {},
+        spotY = {};
+      spotX.lng = this.Xstart.lng;
+      spotX.lat = item.latitude;
+      let left = this.locate(this.Xstart, spotX);
+      spotY.lng = item.longitude;
+      spotY.lat = this.Ystart.lat;
+      let top = this.locate(this.Ystart, spotY);
       let result = {
-        top:left*width/x,
-        left:top*width/y
-      }
-      return result
+        top: left * width / x,
+        left: top * width / y
+      };
+      return result;
     },
     bindTab(url) {
       wx.navigateTo({ url: url });
     },
     viewDetail(item) {
-      const index = this.activeSpot<0?0:this.activeSpot
-      wx.navigateTo({ url: '../list/main?spot_index=' + index});
+      const index = this.activeSpot < 0 ? 0 : this.activeSpot;
+      wx.navigateTo({ url: "../list/main?spot_index=" + index });
     },
     showWindow(index) {
-      this.activeWindow == index ? this.activeWindow = -1 : this.activeWindow = index;
-      this.activeSpot == index ? this.activeSpot = -1 : this.activeSpot = index;
-      if(index<12){
-        this.x = -1400
-        this.y = -900
-      }else if(index<15 && index>11){
-        this.x = -300
-        this.y = -1900
-      }else if(index==15){
-        this.x = -1199
-        this.y = -247
-      }else if(index>15&&index<=33){
-        this.x = -176
-        this.y = -455
-      }else if(index>33&&index<=40){
-        this.x = -307
-        this.y = -138
-      }else if(index>40&&index<=51){
-        this.x = -485
-        this.y = -276
-      }else if(index>51&&index<=59){
-        this.x = -590
-        this.y = -676
-      }else if(index>59&&index<=66){
-        this.x = -772
-        this.y = -739
-      }else if(index>66&&index<=73){
-        this.x = -1022
-        this.y = -384
-      }else if(index>73&&index<=81){
-        this.x = -1199
-        this.y = -247
-      }else if(index==82){
-        this.x = -176
-        this.y = -455
+      this.activeWindow == index
+        ? (this.activeWindow = -1)
+        : (this.activeWindow = index);
+      this.activeSpot == index
+        ? (this.activeSpot = -1)
+        : (this.activeSpot = index);
+      if (index < 12) {
+        this.x = -1400;
+        this.y = -900;
+      } else if (index < 15 && index > 11) {
+        this.x = -300;
+        this.y = -1900;
+      } else if (index == 15) {
+        this.x = -1199;
+        this.y = -247;
+      } else if (index > 15 && index <= 33) {
+        this.x = -176;
+        this.y = -455;
+      } else if (index > 33 && index <= 40) {
+        this.x = -307;
+        this.y = -138;
+      } else if (index > 40 && index <= 51) {
+        this.x = -485;
+        this.y = -276;
+      } else if (index > 51 && index <= 59) {
+        this.x = -590;
+        this.y = -676;
+      } else if (index > 59 && index <= 66) {
+        this.x = -772;
+        this.y = -739;
+      } else if (index > 66 && index <= 73) {
+        this.x = -1022;
+        this.y = -384;
+      } else if (index > 73 && index <= 81) {
+        this.x = -1199;
+        this.y = -247;
+      } else if (index == 82) {
+        this.x = -176;
+        this.y = -455;
       }
     },
-    locate(point1,point2) {
-      function rad(d){
-        return d * Math.PI / 180.0;//经纬度转换成三角函数中度分表形式。
+    locate(point1, point2) {
+      function rad(d) {
+        return d * Math.PI / 180.0; //经纬度转换成三角函数中度分表形式。
       }
       let radLat1 = rad(point1.lat);
       let radLat2 = rad(point2.lat);
       let a = radLat1 - radLat2;
       let b = rad(point1.lng) - rad(point2.lng);
-      let s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+      let s =
+        2 *
+        Math.asin(
+          Math.sqrt(
+            Math.pow(Math.sin(a / 2), 2) +
+              Math.cos(radLat1) *
+                Math.cos(radLat2) *
+                Math.pow(Math.sin(b / 2), 2)
+          )
+        );
       s = s * 6378.137;
       // EARTH_RADIUS;
       s = Math.round(s * 10000) / 10000;
       // console.log(s)
-      return s
+      return s;
     },
     getSpot() {
-      const self = this
-      const storageData = wx.getStorageSync('NatureList')
-      if(storageData){
-        storageData.forEach((item,index) => {
-          let result = this.distance(item)
-            item.top = result.top
-            item.left = result.left
-          });
-        this.spotList = storageData
-        return
+      const self = this;
+      const storageData = wx.getStorageSync("NatureList");
+      if (storageData) {
+        storageData.forEach((item, index) => {
+          let result = this.distance(item);
+          item.top = result.top;
+          item.left = result.left;
+        });
+        this.spotList = storageData;
+        return;
       }
       wx.request({
-        url: config.base + 'attraction/NaturalList', //开发者服务器接口地址",
+        url: config.base + "attraction/NaturalList", //开发者服务器接口地址",
         data: {
-          lineId:config.lineId
+          lineId: config.lineId
         }, //请求的参数",
-        method: 'GET',
-        dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+        method: "GET",
+        dataType: "json", //如果设为json，会尝试对返回的数据做一次 JSON.parse
         success: res => {
           // console.log(res)
-          let data = res.data.data
-          this.setStorage('NatureList',data)
+          let data = res.data.data;
+          this.setStorage("NatureList", data);
           data.forEach(item => {
-            let result = this.distance(item)
-            item.top = result.top
-            item.left = result.left
+            let result = this.distance(item);
+            item.top = result.top;
+            item.left = result.left;
           });
-          this.spotList = data
+          this.spotList = data;
         },
         fail: () => {},
         complete: () => {}
@@ -214,15 +226,12 @@ export default {
     }
   },
   created() {
-    this.getSpot()
+    this.getSpot();
   },
-  mounted() {
-
-  },
+  mounted() {},
   onReady() {
-    
-    this.x = -1400
-    this.y = -900
+    this.x = -1400;
+    this.y = -900;
   }
 };
 </script>
@@ -234,19 +243,19 @@ export default {
   justify-content: center;
   align-items: center;
 }
-#mid{
+#mid {
   width: 10px;
   height: 10px;
   background: red;
   position: absolute;
   top: 1000rpx;
-  left:430rpx;
+  left: 430rpx;
   z-index: 99999;
 }
 .container {
   width: 100%;
   position: relative;
-  z-index:2;
+  z-index: 2;
   margin-top: 20rpx;
 }
 .map-tab {
@@ -275,40 +284,41 @@ export default {
   width: auto;
   height: auto;
   background: #d1a77f;
-  .mapImg{
+  .mapImg {
     width: 3753rpx;
     height: 3753rpx;
   }
 }
-.spot-window{
+.spot-window {
   position: absolute;
-  z-index:999;
+  z-index: 999;
   width: 248rpx;
   height: 248rpx;
   border: 6rpx solid #bc8d5d;
   // background: #00baea;
-  background: url('https://gw.alicdn.com/tfs/TB1Mxiei4TpK1RjSZR0XXbEwXXa-248-248.png') no-repeat center/cover;
+  background: url("https://gw.alicdn.com/tfs/TB1Mxiei4TpK1RjSZR0XXbEwXXa-248-248.png")
+    no-repeat center/cover;
   border-radius: 40rpx;
   display: flex;
   flex-direction: column;
   align-items: center;
-  &-text{
+  &-text {
     font-size: 26rpx;
-    color:#fff;
+    color: #fff;
     line-height: 90rpx;
   }
-  &-img{
+  &-img {
     width: 220rpx;
     height: 144rpx;
     border-radius: 24rpx;
   }
 }
-.active-spot{
+.active-spot {
   width: 40rpx;
   height: 30rpx;
-  margin:auto;
+  margin: auto;
 }
-.spot-icon{
+.spot-icon {
   width: 36rpx;
   height: 36rpx;
   line-height: 36rpx;
@@ -316,16 +326,16 @@ export default {
   position: absolute;
   z-index: 888;
   text-align: center;
-  color:#292770;
-  font-size:28rpx;
+  color: #292770;
+  font-size: 28rpx;
   border-radius: 50%;
-  border:1px solid #a8368e;
+  border: 1px solid #a8368e;
   // border:1px solid yellow;
 }
-.changeBG{
+.changeBG {
   background: #a8368e;
-  color:#fff;
-  border:yellow;
+  color: #fff;
+  border: yellow;
 }
 .index-tab {
   position: fixed;
@@ -335,25 +345,25 @@ export default {
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
-  z-index:21;
+  z-index: 21;
   &-item {
     width: 100rpx;
     height: 110rpx;
-    margin-bottom:36rpx;
+    margin-bottom: 36rpx;
     position: relative;
     z-index: 23;
   }
   &-line {
     width: 1px;
-    height:90%;
-    background-image: url('https://gw.alicdn.com/tfs/TB1B10KmFzqK1RjSZSgXXcpAVXa-2-762.png');
+    height: 90%;
+    background-image: url("https://gw.alicdn.com/tfs/TB1B10KmFzqK1RjSZSgXXcpAVXa-2-762.png");
     background-repeat: repeat-y;
     background-size: cover;
     background-position: center;
     position: absolute;
     top: 10rpx;
     right: 32rpx;
-    z-index:22;
+    z-index: 22;
   }
   img {
     width: 100%;
@@ -361,41 +371,42 @@ export default {
     display: block;
   }
 }
-.spot-item-window{
+.spot-item-window {
   width: 522rpx;
   height: 176rpx;
-  border-bottom:1px solid #c7c7c7;
-  background: url('https://gw.alicdn.com/tfs/TB1PHRpnCzqK1RjSZPxXXc4tVXa-1809-607.png') no-repeat center/contain;
-  position:absolute;
+  border-bottom: 1px solid #c7c7c7;
+  background: url("https://gw.alicdn.com/tfs/TB1PHRpnCzqK1RjSZPxXXc4tVXa-1809-607.png")
+    no-repeat center/contain;
+  position: absolute;
   bottom: 60rpx;
   left: -90rpx;
-  z-index:30;
+  z-index: 30;
   display: flex;
-  &-pic{
+  &-pic {
     width: 136rpx;
     height: 134rpx;
     background: #fff;
-    margin:12rpx 12rpx 0 32rpx;
+    margin: 12rpx 12rpx 0 32rpx;
   }
-  &-text{
+  &-text {
     width: 280rpx;
     display: flex;
     flex-direction: column;
-    color:#101010;
+    color: #101010;
     text-align: left;
     overflow: hidden;
   }
-  &-title{
+  &-title {
     margin-top: 45rpx;
     font-size: 28rpx;
-    line-height:40rpx;
-    white-space:nowrap;
+    line-height: 40rpx;
+    white-space: nowrap;
     overflow: hidden;
   }
-  &-desc{
+  &-desc {
     font-size: 20rpx;
-    color:#6f6f6f;
-    line-height:30rpx;
+    color: #6f6f6f;
+    line-height: 30rpx;
   }
 }
 </style>
